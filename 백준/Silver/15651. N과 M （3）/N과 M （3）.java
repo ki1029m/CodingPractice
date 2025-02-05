1c25ef6b608d22fe0d1d5748f69ef47ba367b11d
김현진 <48575838+ki1029m@users.noreply.github.com>
2025-02-05 13:16:21 +0900

    [Silver III] Title: N과 M (3), Time: 480 ms, Memory: 68888 KB -BaekjoonHub

--- 백준/Silver/15651. N과 M （3）/N과 M （3）.java
+++ 백준/Silver/15651. N과 M （3）/N과 M （3）.java
@@ -1,38 +1,40 @@
 import java.io.*;
-import java.util.StringTokenizer;
- 
+import java.util.*;
+
 public class Main {
-    static int N, M;
-    static int[] arr;
-    static StringBuilder sb = new StringBuilder();
-    
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st = new StringTokenizer(br.readLine());
-        N = Integer.parseInt(st.nextToken());
-        M = Integer.parseInt(st.nextToken());
-        
-        arr = new int[N];
-        for (int i = 0; i < N; i++) {
-            arr[i] = i + 1;
-        }
-        
-        recursive(0, new int[M]);
-        System.out.print(sb);
-    }
-    
-    static void recursive(int depth, int[] result) {
-        if (depth == M) {
-            for (int i = 0; i < M; i++) {
+
+	static int[] arr;
+	static int N;
+	static int M;
+	static StringBuilder sb = new StringBuilder();
+	
+	public static void recursive(int[] result, int idx) {
+		if(idx == M) {
+			for (int i = 0; i < M; i++) {
                 sb.append(result[i]).append(" ");
             }
             sb.append("\n");
-            return;
-        }
-        
-        for (int i = 1; i <= N; i++) {
-            result[depth] = arr[i - 1];
-            recursive(depth + 1, result);
-        }
-    }
+			return;
+		}
+		for(int i=0; i<N; i++) {
+			result[idx] = arr[i];
+			recursive(result, idx+1);
+		}
+	}
+	
+	public static void main(String[] args) throws IOException{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		N = Integer.parseInt(st.nextToken());
+		M = Integer.parseInt(st.nextToken());
+		
+		arr = new int[N];
+		for(int i=0; i<N; i++) {
+			arr[i]=i+1;
+		}
+		
+		recursive(new int[M], 0);
+		System.out.print(sb);
+	}
+
 }
