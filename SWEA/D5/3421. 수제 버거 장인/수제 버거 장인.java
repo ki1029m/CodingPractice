4b911790abab1dbda6bbaa247f63ad63ebef415a
김현진 <48575838+ki1029m@users.noreply.github.com>
2025-02-21 11:48:43 +0900

    [D5] Title: 수제 버거 장인, Time: 185 ms, Memory: 54,784 KB -BaekjoonHub

---
+++ SWEA/D5/3421. 수제 버거 장인/수제 버거 장인.java
@@ -0,0 +1,59 @@
+import java.io.*;
+import java.util.*;
+
+public class Solution {
+	
+	static class Ingredient{
+		int num1;
+		int num2;
+		Ingredient(int num1, int num2){
+			this.num1=num1;
+			this.num2=num2;
+		}
+	}
+
+	static void powerSet(int depth, int state) {
+		
+		for(Ingredient i : q) {
+			if( (state & (1<<(i.num1-1)))!=0 && (state & (1<<(i.num2-1)))!=0 ) {
+				return;
+			}
+		}
+		
+		if(depth==N) {
+			
+			result++;
+			return;
+		}
+		
+		powerSet(depth+1, (state | (1<<depth)));
+		powerSet(depth+1, state);
+		
+	}
+	
+	static int N,M,result;
+	static Deque<Ingredient> q;
+	
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int T = Integer.parseInt(br.readLine());
+		for(int t=1; t<=T; t++) {
+			StringTokenizer st = new StringTokenizer(br.readLine());
+			N = Integer.parseInt(st.nextToken());
+			M = Integer.parseInt(st.nextToken());
+			result = 0;
+			q = new ArrayDeque<>();
+			for(int i=0; i<M; i++) {
+				st = new StringTokenizer(br.readLine());
+				int i1 = Integer.parseInt(st.nextToken());
+				int i2 = Integer.parseInt(st.nextToken());
+				q.add(new Ingredient(i1,i2));
+			}
+			
+			powerSet(0,0);
+			System.out.println("#"+t+" "+result);
+		}
+		
+	}
+
+}
