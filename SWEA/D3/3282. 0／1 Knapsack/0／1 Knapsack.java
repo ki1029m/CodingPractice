4e758f1524a0b58b731d2b3c92afe88a464467b4
김현진 <48575838+ki1029m@users.noreply.github.com>
2025-04-07 11:58:16 +0900

    [D3] Title: 0/1 Knapsack, Time: 118 ms, Memory: 28,140 KB -BaekjoonHub

--- SWEA/D3/3282. 0／1 Knapsack/0／1 Knapsack.java
+++ SWEA/D3/3282. 0／1 Knapsack/0／1 Knapsack.java
@@ -12,8 +12,8 @@ public class Solution {
 			int N = Integer.parseInt(st.nextToken());
 			int K = Integer.parseInt(st.nextToken());
 			
-			int[] V = new int[N];
-			int[] C = new int[N];
+			int[] V = new int[N]; //부피
+			int[] C = new int[N]; //가치
 			
 			for(int i=0; i<N; i++) {
 				st = new StringTokenizer(br.readLine());
@@ -23,7 +23,7 @@ public class Solution {
 			
 			int[] dp = new int[K+1];
 			for(int i=0; i<N; i++) {
-				for(int v=K; v>0; v--) {
+				for(int v=K; v>=0; v--) {
 					if(V[i] <= v) {
 						dp[v] = Math.max(dp[v], dp[v-V[i]] + C[i]);
 					}
